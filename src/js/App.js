375c28b8346eaa72e238f90846b57c4b6c645aec
maximkasatkin <cmIMav0mv0>
2019-02-27 13:45:24 +0700

    dev

--- src/js/App.js
+++ src/js/App.js
@@ -2,6 +2,7 @@ const App = {
   init: function() {
     this.initMobileMenu()
     this.initSearch()
+    this.initGallery()
     this.initFeedback()
   },
 
@@ -33,6 +34,26 @@ const App = {
         $block.find('input').focus().val('')
       }
     })
+  },
+
+  initGallery: function() {
+    $('.js-gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        titleSrc: function(item) {
+          return item.el.attr('title');
+        }
+      }
+    });
   },
 
   initFeedback: function() {
